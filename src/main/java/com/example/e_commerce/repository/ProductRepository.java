469b4e38ac19ed120de28c9f7370d250b118308e
factor-4 <snk.sojib@gmail.com>
2025-11-27 22:50:16 +0200

    get-delete-search products

--- src/main/java/com/example/e_commerce/repository/ProductRepository.java
+++ src/main/java/com/example/e_commerce/repository/ProductRepository.java
@@ -1,8 +1,18 @@
 package com.example.e_commerce.repository;
 
+import com.example.e_commerce.dto.ProductResponse;
 import com.example.e_commerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository <Product, Long> {
 
+    List<Product> findByActiveTrue();
+
+    @Query("SELECT p FROM products p WHERE p.active = true AND p.stockQuantity > 0  AND LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(@Param("keyword") String keyword);
 }
--- src/main/java/com/example/e_commerce/service/ProductService.java
+++ src/main/java/com/example/e_commerce/service/ProductService.java
@@ -7,7 +7,9 @@ import com.example.e_commerce.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +32,7 @@ public class ProductService {
         response.setPrice(savedProduct.getPrice());
         response.setImageUrl(savedProduct.getImageUrl());
         response.setStockQuantity(savedProduct.getStockQuantity());
+        response.setDescription(savedProduct.getDescription());
         return response;
     }
 
@@ -39,6 +42,7 @@ public class ProductService {
         product.setPrice(productRequest.getPrice());
         product.setImageUrl(productRequest.getImageUrl());
         product.setStockQuantity(productRequest.getStockQuantity());
+        product.setDescription(productRequest.getDescription());
     }
 
     public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest) {
@@ -49,5 +53,29 @@ public class ProductService {
                     return mapToProductResponse(savedProduct);
                 });
 
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findByActiveTrue().stream()
+                .map(this::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+
+    public boolean deleteProduct(Long id) {
+
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setActive(false);
+                    productRepository.save(product);
+                    return true;
+                }).orElse(false);
+
+
+    }
+
+    public List<ProductResponse> searchProducts(String keyword) {
+        return productRepository.searchProducts(keyword).stream()
+                .map(this::mapToProductResponse)
+                .collect(Collectors.toList());
     }
 }
